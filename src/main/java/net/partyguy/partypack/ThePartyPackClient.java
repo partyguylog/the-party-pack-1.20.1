6ff73dc011c7a42099ce0353db25a3e7ae20fb8a
partyguylog <jthomas12j@gmail.com>
2024-06-24 13:29:32 +0800

    Initial Commit

---
+++ src/main/java/net/partyguy/partypack/ThePartyPackClient.java
@@ -0,0 +1,10 @@
+package net.partyguy.partypack;
+
+import net.fabricmc.api.ClientModInitializer;
+public class ThePartyPackClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
